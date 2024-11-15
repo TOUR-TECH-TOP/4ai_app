b3fd4e94368510965645553bc3ffcdef8f9d4b84
Anandhi <anandhi@tourtech.co.il>
2024-11-15 11:23:10 +0530

    feat - added audio support to webview

--- chatsdk4ai/src/main/java/com/aichat/chatsdk/ChatActivity.kt
+++ chatsdk4ai/src/main/java/com/aichat/chatsdk/ChatActivity.kt
@@ -1,11 +1,15 @@
 package com.aichat.chatsdk
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +19,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class ChatActivity : ComponentActivity() {
 
@@ -27,12 +33,22 @@ class ChatActivity : ComponentActivity() {
         }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val siteId = intent.getStringExtra(SITE_ID)
 
         setContent {
+
+            checkAndRequestAudioPermission(this)
             MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -44,6 +60,16 @@ class ChatActivity : ComponentActivity() {
         }
     }
 }
+
+private fun checkAndRequestAudioPermission(context: ChatActivity) {
+    if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO)
+        != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(
+            context, arrayOf(Manifest.permission.RECORD_AUDIO), 10
+        )
+    }
+}
+
 
 @Composable
 fun WebViewScreen(siteId: String?) {
@@ -60,16 +86,37 @@ fun WebViewScreen(siteId: String?) {
                         super.onPageFinished(view, url)
                     }
                 }
-                webChromeClient = WebChromeClient()
+                webChromeClient = object : WebChromeClient() {
+                    override fun onPermissionRequestCanceled(request: PermissionRequest?) {
+                        super.onPermissionRequestCanceled(request)
+                        Toast.makeText(context,"Canceled", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onPermissionRequest(request: PermissionRequest?) {
+                        val requestedResources = request?.resources
+                        if (requestedResources != null) {
+                            for (r in requestedResources) {
+                                if (r == PermissionRequest.RESOURCE_AUDIO_CAPTURE) {
+                                    request.grant(arrayOf(PermissionRequest.RESOURCE_AUDIO_CAPTURE))
+                                    break
+                                }
+                            }
+                        }
+                    }
+                }
+                settings.domStorageEnabled = true
                 settings.javaScriptEnabled = true
-//                settings.loadWithOverviewMode = true
-//                settings.useWideViewPort = true
+                settings.mediaPlaybackRequiresUserGesture = false
+                settings.allowFileAccess = true
+                settings.allowContentAccess = true
+            //                settings.useWideViewPort = true
 //                settings.builtInZoomControls = true
 //                settings.displayZoomControls = false
             }
         },
         update = { webView ->
-            webView.loadDataWithBaseURL(null, htmlContent, "text/html", "utf-8", null)
+            webView.loadUrl("https://4ai.chat/mobile-script?siteId=${siteId}")
+//            webView.loadDataWithBaseURL(null, htmlContent, "text/html", "utf-8", null)
         }
     )
 
